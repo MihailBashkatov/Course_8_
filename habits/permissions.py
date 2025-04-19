fc1baf95cc6ba9968cbce47d8e0c1d56c0a6b7c7
mihail <mihail.bashkatov@gmail.com>
2025-04-19 13:24:44 +0200

    Adding permission IsOwner in habits/permissions.py

---
+++ habits/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework import permissions
+
+
+class IsOwner(permissions.BasePermission):
+    """
+    Object-level permission to only allow user of an object(habit) to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+
+        if obj.habit_user == request.user:
+            return True
+        return False
