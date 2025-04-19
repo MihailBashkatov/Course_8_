71912fb545a4681bd231f35369ef8387ad6e51c6
mihail <mihail.bashkatov@gmail.com>
2025-04-19 13:31:07 +0200

    Adding HabitSerializer in habits/serializers.py

---
+++ habits/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from habits.models import Habit
+
+
+class HabitSerializer(serializers.ModelSerializer):
+    """Serializer for the model Habit."""
+
+    class Meta:
+        model = Habit
+        fields = "__all__"
