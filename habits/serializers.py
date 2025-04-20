956c2958f5fbe090dd700a987ee36cee595bb843
mihail <mihail.bashkatov@gmail.com>
2025-04-20 18:20:21 +0200

    Deleting NiceHabitsSerializer

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,20 +1,10 @@
 from rest_framework import serializers
 
-from habits.models import Habit, NiceHabit
-
-
-class NiceHabitSerializer(serializers.ModelSerializer):
-    """Serializer for the model NiceHabit."""
-
-
-    class Meta:
-        model = NiceHabit
-        fields = "__all__"
+from habits.models import Habit
 
 
 class HabitSerializer(serializers.ModelSerializer):
     """Serializer for the model Habit."""
-    nice_habit = NiceHabitSerializer(read_only=True)
 
     class Meta:
         model = Habit
