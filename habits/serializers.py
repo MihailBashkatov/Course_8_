b4905690db02e9d6c7c0afca3cc237acc6c5818a
mihail <mihail.bashkatov@gmail.com>
2025-04-20 11:04:37 +0200

    Adding NiceHabitSerializer in habits/serializers.py

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,11 +1,21 @@
 from rest_framework import serializers
 
-from habits.models import Habit
+from habits.models import Habit, NiceHabit
+
+
+class NiceHabitSerializer(serializers.ModelSerializer):
+    """Serializer for the model NiceHabit."""
+
+
+    class Meta:
+        model = NiceHabit
+        fields = "__all__"
 
 
 class HabitSerializer(serializers.ModelSerializer):
     """Serializer for the model Habit."""
+    nice_habit = NiceHabitSerializer(read_only=True)
 
     class Meta:
         model = Habit
-        fields = "__all__"
+        fields = '__all__'
