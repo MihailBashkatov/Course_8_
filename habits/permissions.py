5f5682e57e791726cdf4a435f81d1817f3c2b5f2
mihail <mihail.bashkatov@gmail.com>
2025-04-20 11:05:57 +0200

    Adding IsOwnerNiceHabit permissions in habit/permissions.py

--- habits/permissions.py
+++ habits/permissions.py
@@ -11,3 +11,15 @@ class IsOwner(permissions.BasePermission):
         if obj.habit_user == request.user:
             return True
         return False
+
+
+class IsOwnerNiceHabit(permissions.BasePermission):
+    """
+    Object-level permission to only allow user of an object(habit) to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+
+        if obj.nice_habit_user == request.user:
+            return True
+        return False
