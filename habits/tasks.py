31a61f126f08b0311b7c6ef0529e29a7eceeec17
mihail <mihail.bashkatov@gmail.com>
2025-05-09 23:53:17 +0200

    Adding send_reminder in habits/tasks.py

--- habits/tasks.py
+++ habits/tasks.py
@@ -1,6 +1,15 @@
 from celery import shared_task
 
+from habits.services import send_telegram_message
+from users.models import User
+
 
 @shared_task
-def add():
-    print("Hello, world")
+def send_reminder(email):
+    """Task to send Telegram message, if user chose option to get telegram messages"""
+
+    message = 'Hello, world"'
+    user = User.objects.get(email=email)
+    if user.telegram_chat_id:
+        telegram_chat_id = user.telegram_chat_id
+        send_telegram_message(telegram_chat_id, message)
