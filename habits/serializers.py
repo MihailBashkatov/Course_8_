d9b5aff45e0e89663152d16509e42f0fbfaa2443
mihail <mihail.bashkatov@gmail.com>
2025-05-09 17:22:13 +0200

    Adding validators for HabitSerializer in habits/serializers.py

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,6 +1,10 @@
 from rest_framework import serializers
 
 from habits.models import Habit
+from habits.validators import (HabitPeriodValidator, NiceHabitRelatedValidator,
+                               NiceNotRewardNotRelatedHabitValidator,
+                               RewardOrHabitRelatedValidator,
+                               TimeDurationValidator)
 
 
 class HabitSerializer(serializers.ModelSerializer):
@@ -8,4 +12,11 @@ class HabitSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Habit
-        fields = '__all__'
+        fields = "__all__"
+        validators = [
+            TimeDurationValidator(field=["habit_time_duration"]),
+            RewardOrHabitRelatedValidator(field=["related_habit", "habit_reward"]),
+            NiceHabitRelatedValidator(field=["related_habit"]),
+            NiceNotRewardNotRelatedHabitValidator(field=["is_nice_habit"]),
+            HabitPeriodValidator(field=["habit_period"]),
+        ]
